8818ed682f2ce00bcb7972d732ec15ad78e2cd10
devanshiteotia162 <teotia.devanshi2102@gmail.com>
2026-03-15 00:55:45 +0530

    index 2

--- script.js
+++ script.js
@@ -27,42 +27,42 @@
 //    console.log("false");
 // }
 
-var a = 10; var b = 20; 
- var linebreak = "<br>";
+// var a = 10; var b = 20; 
+//  var linebreak = "<br>";
 
-document.write("(a==b)=>");
-result = (a==b);
-document.write(result);
-document.write(linebreak);
-document.write("(a<b)=>");
-result = (a<b);
-document.write(result);
-document.write(linebreak);
-document.write("(a>b)=>");
-result = (a>b);
-document.write(result);
-document.write(linebreak);
+// document.write("(a==b)=>");
+// result = (a==b);
+// document.write(result);
+// document.write(linebreak);
+// document.write("(a<b)=>");
+// result = (a<b);
+// document.write(result);
+// document.write(linebreak);
+// document.write("(a>b)=>");
+// result = (a>b);
+// document.write(result);
+// document.write(linebreak);
 
 
 
-var count ;
-document.write("Starting Loop"+"</br>");
-for (count =0 ; count<10 ; count ++ ){
- document.write("Current count :"+count  )
- document.write("</br>");
+// var count ;
+// document.write("Starting Loop"+"</br>");
+// for (count =0 ; count<10 ; count ++ ){
+//  document.write("Current count :"+count  )
+//  document.write("</br>");
 
- }
- document.write("Loop stopped !");
- <p>Set the varibles to different values and then try </p>
- const person ={
-    fname : "Devanshi" ,
-    lname : "Teotia" ,
-    age : 19 ,
-};
-for ( let x in person ){
-    console.log("person details : " +x+";" +person [x])
-}
-
+//  }
+//  document.write("Loop stopped !");
+//  <p>Set the varibles to different values and then try </p>
+//  const person ={
+//     fname : "Devanshi" ,
+//     lname : "Teotia" ,
+//     age : 19 ,
+// };
+// for ( let x in person ){
+//     console.log("person details : " +x+";" +person [x])
+// }
+console.log("Hello Devanshi!");
 
 
 
