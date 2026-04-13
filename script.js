ce6ab4dcbf746480385757f271c3b75829da0ebb
devanshiteotia162 <teotia.devanshi2102@gmail.com>
2026-04-13 22:20:42 +0530

    javascript

--- script.js
+++ script.js
@@ -1,67 +1,67 @@
-// console.log(" Hello World");
+console.log(" Hello World");
 
-// var num = 10;
-// console.log(num);
-// console.log(typeof(num));
-// num=false;
-// console.log(num);
-// console.log(typeof(num));
-// num="Devanshi Teotia";
-// console.log(num);
-// console.log(typeof(num));
-// num=null;
-// console.log(num);
-// console.log(typeof(num));
-// num=undefined;
-// console.log(num);
-// console.log(typeof(num));
+var num = 10;
+console.log(num);
+console.log(typeof(num));
+num=false;
+console.log(num);
+console.log(typeof(num));
+num="Devanshi Teotia";
+console.log(num);
+console.log(typeof(num));
+num=null;
+console.log(num);
+console.log(typeof(num));
+num=undefined;
+console.log(num);
+console.log(typeof(num));
 
-// const sym1 = Symbol(4);
-// console.log(sym1);
-// const sym2 = Symbol(4);
-// console.log(sym2);
-// if (sym1==sym2){
-//     console.log("true");
-// }
-// else{
-//    console.log("false");
-// }
+const sym1 = Symbol(4);
+console.log(sym1);
+const sym2 = Symbol(4);
+console.log(sym2);
+if (sym1==sym2){
+    console.log("true");
+}
+else{
+   console.log("false");
+}
 
-// var a = 10; var b = 20; 
-//  var linebreak = "<br>";
+var a = 10; var b = 20; 
+ var linebreak = "<br>";
 
-// document.write("(a==b)=>");
-// result = (a==b);
-// document.write(result);
-// document.write(linebreak);
-// document.write("(a<b)=>");
-// result = (a<b);
-// document.write(result);
-// document.write(linebreak);
-// document.write("(a>b)=>");
-// result = (a>b);
-// document.write(result);
-// document.write(linebreak);
+document.write("(a==b)=>");
+result = (a==b);
+document.write(result);
+document.write(linebreak);
+document.write("(a<b)=>");
+result = (a<b);
+document.write(result);
+document.write(linebreak);
+document.write("(a>b)=>");
+result = (a>b);
+document.write(result);
+document.write(linebreak);
 
 
 
-// var count ;
-// document.write("Starting Loop"+"</br>");
-// for (count =0 ; count<10 ; count ++ ){
-//  document.write("Current count :"+count  )
-//  document.write("</br>");
+var count ;
+document.write("Starting Loop"+"</br>");
+for (count =0 ; count<10 ; count ++ ){
+ document.write("Current count :"+count  )
+ document.write("</br>");
 
-//  }
-//  document.write("Loop stopped !");
-//  <p>Set the varibles to different values and then try </p>
-//  const person ={
-//     fname : "Devanshi" ,
-//     lname : "Teotia" ,
-//     age : 19 ,
-// };
-// for ( let x in person ){
-//     console.log("person details : " +x+";" +person [x])
-// }
+ }
+ document.write("Loop stopped !");
+ <p>Set the varibles to different values and then try </p>
+ const person ={
+    fname : "Devanshi" ,
+    lname : "Teotia" ,
+    age : 19 ,
+};
+for ( let x in person ){
+    console.log("person details : " +x+";" +person [x])
+}
 console.log("Hello Devanshi!");
 
 
