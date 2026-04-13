f07544d1c3eeeefd80111cf025f41f698b25c1ca
devanshiteotia162 <teotia.devanshi2102@gmail.com>
2026-04-13 22:08:31 +0530

    index4

---
+++ script4.js
@@ -0,0 +1,26 @@
+const element = document.createElement("h2");
+const bodytag = document.getElementsByTagName("body");
+// node.prepend ()= add at the start of the node 
+// node.before()= add before the node 
+// node.after ()= add after the node 
+// node.appennd() = add at the end of the node 
+// the change in state of an object is known as an event 
+// mouse events 
+// click == onclick 
+// mouseover == onmouseover 
+// mouseout == onmouseout
+// mousedown == onmousedown 
+// mouseup == onmouseup 
+// mousemove == onmousemove
+// on double click = ondblclick
+// key board events  -> keydown == onkeydown ; keyup == onkeyup 
+// form eventss 
+// focus == onfocus 
+// submit ; blur ; change ;
+function onMouseOver (){
+
+}
+function onhandlefocus (){ x .style.background = "red ";
+
+}
+function onhandleblur (){x .style.background = "red ";}
